bb04497d6b0899024353de50237f6e9563fc2e2a
Rifqi Pratama <rifqipratama9@gmail.com>
2022-03-22 18:17:33 +0700

    change to image magick

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 let revealContainer = document.querySelectorAll(".reveal");
 let stackContainer = document.querySelector(".stack-container");
@@ -10,7 +10,17 @@ const imageScroll = document.querySelector('.scroll-magic');
 //   console.log(stackContainer);
 // });
 
-stackContainer.style.position = "fixed";
+// window.addEventListener('scroll', function(e) {
+//     var bottom = window.height - imageScroll.height;
+
+//     if(window.scrollTop > imageScroll.offsetTop) {
+//         imageScroll.style.position = 'relative';
+//     } else {
+//         stackContainer.style.position = 'fixed';
+//     }
+
+//     console.log(bottom.offsetTop - bottom);
+// })
 
 // gsap.from(".reveal-1", {
 //   opacity: 1,
@@ -22,39 +32,39 @@ stackContainer.style.position = "fixed";
 //   },
 // });
 
-gsap.to("#reveal-1", {
-  autoAlpha: 1,
-  yPercent: -100,
-  transformOrigin: "0% 150%",
-  scrollTrigger: {
-    trigger: ".scroll-magic-image-first",
-    start: "top top",
-    end: "bottom top",
-    scrub: true,
-  },
-});
+// gsap.to("#reveal-1", {
+//   autoAlpha: 1,
+//   yPercent: -100,
+//   transformOrigin: "0% 150%",
+//   scrollTrigger: {
+//     trigger: ".scroll-magic-image-first",
+//     start: "top top",
+//     end: "bottom top",
+//     scrub: true,
+//   },
+// });
 
-gsap.to("#reveal-2", {
-  autoAlpha: 1,
-  yPercent: -100,
-  scrollTrigger: {
-    trigger: "#img-02",
-    start: "top top",
-    end: "bottom top",
-    scrub: true,
-  },
-});
+// gsap.to("#reveal-2", {
+//   autoAlpha: 1,
+//   yPercent: -100,
+//   scrollTrigger: {
+//     trigger: "#img-02",
+//     start: "top top",
+//     end: "bottom top",
+//     scrub: true,
+//   },
+// });
 
-gsap.to("#reveal-3", {
-  autoAlpha: 1,
-  yPercent: -100,
-  scrollTrigger: {
-    trigger: "#img-02",
-    start: "top top",
-    end: "bottom top",
-    scrub: true,
-  },
-});
+// gsap.to("#reveal-3", {
+//   autoAlpha: 1,
+//   yPercent: -100,
+//   scrollTrigger: {
+//     trigger: "#img-02",
+//     start: "top top",
+//     end: "bottom top",
+//     scrub: true,
+//   },
+// });
 
 
 
@@ -97,3 +107,72 @@ gsap.to("#reveal-3", {
 
 //     // console.log(revealContainer);
 // })
+
+// const controller = ScrollMagic.Controller();
+
+// let tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".scroll-magic",
+//     start: "top top",
+//     end: "bottom bottom",
+//     scrub: 0.8,
+//     invalidateOnRefresh: true,
+//   },
+// });
+
+// tl.set("#reveal-1", {
+//     autoAlpha: 1,
+// });
+
+// tl.to("#reveal-1", 1.5, {
+//   autoAlpha: 1,
+//   yPercent: -100,
+//   ease: Power2.out,
+// });
+
+// tl.to("#reveal-2", 1.5, {
+//   autoAlpha: 1,
+//   yPercent: -100,
+//   ease: Power2.out,
+// });
+
+// const scene = new ScrollMagic.Scene({
+//   triggerElement: ".stack-container",
+//   triggerHook: "onLeave",
+//   duration: "100%",
+// }).setPin(".stack-container").setTween(tl).addTo(controller);
+
+var tl = new TimelineMax({ onUpdate: updatePercentage });
+const controller = new ScrollMagic.Controller();
+
+tl.from("#reveal-1", 2, { y: 400, opacity: 0, autoAlpha: 1, ease: Power4.easeInOut });
+tl.from("#reveal-2", 1.5, { y: 250, opacity: 0, ease: Power4.easeInOut });
+tl.from("#reveal-3", 1.5, { y: 250, opacity: 0, ease: Power4.easeInOut });
+tl.from("#reveal-4", 1.5, { y: 100, opacity: 0, ease: Power4.easeInOut });
+tl.from("#reveal-5", 1.5, { y: 100, opacity: 0, ease: Power4.easeInOut });
+
+
+// tl2.from("#box", 1, { opacity: 0, scale: 0 });
+// tl2.to("#box", 0.5, {
+//   left: "20%",
+//   scale: 1.3,
+//   borderColor: "white",
+//   borderWidth: 12,
+//   boxShadow: "1px 1px 0px 0px rgba(0,0,0,0.09)",
+// });
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".stack-container",
+  triggerHook: "onLeave",
+  duration: "100%",
+})
+  .setPin(".stack-container")
+  .setTween(tl)
+  .addTo(controller);
+
+
+function updatePercentage() {
+  //percent.innerHTML = (tl.progress() *100 ).toFixed();
+  tl.progress();
+  console.log(tl.progress());
+}
