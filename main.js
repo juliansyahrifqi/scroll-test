03c265e19301d2f46f148101d516b1d932abefd9
Rifqi Pratama <rifqipratama9@gmail.com>
2022-03-22 13:24:26 +0700

    test

---
+++ main.js
@@ -0,0 +1,83 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let revealContainer = document.querySelectorAll(".reveal");
+let stackContainer = document.querySelector(".stack-container");
+let first = document.querySelector(".scroll-magic-image-first");
+const imageScroll = document.querySelector('.scroll-magic');
+
+// console.log(stackContainer);
+// stackContainer.addEventListener("scroll", function (e) {
+//   console.log(stackContainer);
+// });
+
+stackContainer.style.position = "fixed";
+
+gsap.to(".scroll-magic-image-first", {
+  opacity: 1,
+  scrollTrigger: {
+    trigger: "#img-01",
+    start: "top top",
+    end: "top top",
+    scrub: true,
+  },
+});
+
+gsap.to("#reveal-1", {
+  autoAlpha: 1,
+  yPercent: -100,
+  scrollTrigger: {
+    trigger: "#img-02",
+    scrub: true,
+  },
+});
+
+gsap.to("#reveal-2", {
+  autoAlpha: 1,
+  yPercent: -100,
+  scrollTrigger: {
+    trigger: "#img-03",
+    scrub: true,
+  },
+});
+
+
+
+
+// revealContainer.forEach((stack) => {
+//     let image = stack.querySelector("img");
+
+//     let tl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: stackContainer,
+//         },
+//     });
+
+//     // tl.set(stack, {
+//     //     autoAlpha: 1,
+//     // });
+
+//     tl.to(stack, {
+//         rotation: -270, duration: 1, ease: "elastic"
+//     });
+//     // tl.from(stack, 1.5, {
+//     //     xPercent: 100,
+//     //     ease: Power2.out
+//     // });
+
+//     // tl.from(image, 1.5, {
+//     //     yPercent: 20,
+//     //     scale: 1.3,
+//     //     delay: -1.5,
+//     //     ease: Power2.out
+//     // })
+
+//     // gsap.to(first, {
+//     //     duration: 1,
+//     //     top: 10,
+//     //     width: "100%",
+//     // })
+
+//     stackContainer.style.position = 'fixed';
+
+//     // console.log(revealContainer);
+// })
